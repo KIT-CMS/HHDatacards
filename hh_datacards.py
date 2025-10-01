193aecd023a6b2ee4e194b55518e3b323331ca96
Simon Daigler <simon.daigler@student.kit.edu>
2025-10-01 18:44:17 +0200

    feat: added uncertainty model

--- hh_datacards.py
+++ hh_datacards.py
@@ -2,6 +2,8 @@ import os
 import CombineHarvester.CombineTools.ch as ch
 import argparse
 
+SYSTEMATICS = False
+
 def parse_args():
     p = argparse.ArgumentParser()
     p.add_argument("--ntuple-tag", default=os.environ.get("NTUPLETAG"))
@@ -37,6 +39,7 @@ def main():
         "ST-STT", "ST-STJ", "ST-STL",
         "TTV-TTVJ", "TTV-TTVL", "TTV-TTVT",
         "ggH_htt125", "qqH_htt125", "ttH_htt125", "VH125",
+        "QCD"
     ]
     signal = "HH2B2Tau"
 
@@ -60,7 +63,79 @@ def main():
         cb.cp().channel([fs]).ExtractShapes(synced_shapes_dir, "$BIN/$PROCESS", "$BIN/$PROCESS_$SYSTEMATIC")
 
     # Adding systematic uncertainties
-    # cb.cp().AddSyst(cb,"eff_m","lnN",ch.SystMap()(1.03)) # Example uncertainty
+    if SYSTEMATICS:
+        cb.cp().channel(["et"]).AddSyst(cb,"eff_e","lnN",ch.SystMap()(1.02))
+        cb.cp().channel(["mt"]).AddSyst(cb,"eff_m","lnN",ch.SystMap()(1.02))
+
+        # eff_t only for IDvsEle and IDvsMu, 
+        T_VS_LEP = 1.01
+        cb.cp().channel(["et","mt"]).AddSyst(cb,"eff_t_vsLep_$CHANNEL_$ERA","lnN",ch.SystMap()(T_VS_LEP))
+        cb.cp().channel(["tt"]).AddSyst(cb,"eff_t_vsLep_$CHANNEL_$ERA","lnN",ch.SystMap()(T_VS_LEP * T_VS_LEP))
+
+        # eff_t for IDvsJet (normally as shapes)
+        T_VS_JET = 1.02 # Only a guess
+        cb.cp().channel(["et","mt"]).AddSyst(cb,"eff_t_vsJet_$CHANNEL_$ERA","lnN",ch.SystMap()(T_VS_JET))
+        cb.cp().channel(["tt"]).AddSyst(cb,"eff_t_vsJet_$CHANNEL_$ERA","lnN",ch.SystMap()(T_VS_JET * T_VS_JET))
+
+        # https://twiki.cern.ch/twiki/bin/view/CMS/LumiRecommendationsRun2
+        cb.cp().AddSyst(cb, "lumi_13TeV_$ERA", "lnN", ch.SystMap()(1.025))
+
+        # Uncertainty: Background normalizations
+        # VV 
+        cb.cp().process(["VVT", "VVJ", "VVL"]).AddSyst(cb, "htt_vvXsec", "lnN", ch.SystMap()(1.05))
+        
+        # ST 
+        cb.cp().process(["ST-STT", "ST-STJ", "ST-STL"]).AddSyst(cb, "htt_stXsec", "lnN", ch.SystMap()(1.05))
+
+        # TT
+        cb.cp().process(["TTT", "TTL", "TTJ"]).AddSyst(cb, "htt_tjXsec", "lnN", ch.SystMap()(1.06))
+        
+        # W
+        cb.cp().process(["W"]).AddSyst(cb, "htt_wjXsec", "lnN", ch.SystMap()(1.04))
+
+        # Z
+        cb.cp().process(["ZTT", "ZL", "ZJ"]).AddSyst(cb, "htt_zjXsec", "lnN", ch.SystMap()(1.02))
+        
+        # ttH
+        cb.cp().process(["ttH_htt125"]).AddSyst(cb, "QCDscale_ttH", "lnN", ch.SystMap()((1.058, 0.908)))
+        cb.cp().process(["ttH_htt125"]).AddSyst(cb, "pdf_ttH",      "lnN", ch.SystMap()(1.030))
+        cb.cp().process(["ttH_htt125"]).AddSyst(cb, "alphaS_ttH",   "lnN", ch.SystMap()(1.020))
+
+        # ggH
+        cb.cp().process(["ggH_htt125"]).AddSyst(cb, "QCDscale_ggH", "lnN", ch.SystMap()(1.039))
+        cb.cp().process(["ggH_htt125"]).AddSyst(cb, "pdf_ggH",      "lnN", ch.SystMap()(1.019))
+        cb.cp().process(["ggH_htt125"]).AddSyst(cb, "alphaS_ggH",   "lnN", ch.SystMap()(1.026))
+
+        # qqH
+        cb.cp().process(["qqH_htt125"]).AddSyst(cb, "QCDscale_qqH", "lnN", ch.SystMap()((1.004, 0.997)))
+        cb.cp().process(["qqH_htt125"]).AddSyst(cb, "pdf_qqH",      "lnN", ch.SystMap()(1.021))
+        cb.cp().process(["qqH_htt125"]).AddSyst(cb, "alphaS_qqH",   "lnN", ch.SystMap()(1.005))
+
+        # VH (kombiniert WH + ZH): QCDscale (envelope), pdf (max), alphaS (gemeinsam)
+        cb.cp().process(["VH125"]).AddSyst(cb, "QCDscale_VH", "lnN", ch.SystMap()((1.038, 0.970)))
+        cb.cp().process(["VH125"]).AddSyst(cb, "pdf_VH",      "lnN", ch.SystMap()(1.017))
+        cb.cp().process(["VH125"]).AddSyst(cb, "alphaS_VH",   "lnN", ch.SystMap()(1.009))
+
+        # QCD from AN resolved_2b 
+        # Not for et because no QCD estimated by ABCD method in this channel
+        cb.cp().channel(["et"]).bin_id([2,3,5]).process(["QCD"]).AddSyst(cb, "QCDNorm_$CHANNEL_$ERA", "lnN", ch.SystMap()(1.108))
+        cb.cp().channel(["mt"]).process(["QCD"]).AddSyst(cb, "QCDNorm_$CHANNEL_$ERA", "lnN", ch.SystMap()(1.108))
+        cb.cp().channel(["tt"]).process(["QCD"]).AddSyst(cb, "QCDNorm_$CHANNEL_$ERA", "lnN", ch.SystMap()(1.216))
+
+        # Signal
+        # QCD Scale
+        cb.cp().process([signal]).AddSyst(cb, "QCDscale_HH", "lnN", ch.SystMap()((1.022, 0.95)))
+
+        # PDF+alphaS (gemeinsam, ±3%) – falls getrennt gewünscht, aufsplitten
+        cb.cp().process([signal]).AddSyst(cb, "PDF_alphas_HH", "lnN", ch.SystMap()(1.03))
+
+        # m_top Effekt (±2.6%)
+        cb.cp().process([signal]).AddSyst(cb, "mtop_HH", "lnN", ch.SystMap()(1.026))
+
+        # Branching Fractions für H→bb und H→ττ
+        cb.cp().process([signal]).AddSyst(cb, "BR_h_bb",      "lnN", ch.SystMap()((1.0125, 0.9873)))
+        cb.cp().process([signal]).AddSyst(cb, "BR_h_tautau",  "lnN", ch.SystMap()(1.0165))
+
 
     # Removing processes with yield smaller or equal to 0
     cb.FilterProcs(lambda p : p.rate() <= 0.0)
@@ -73,7 +148,7 @@ def main():
     ch.SetStandardBinNames(cb, "$ANALYSIS_$CHANNEL_$BINID_$ERA")
 
     # Add bin-by-bin (Barlow-Beeston light)
-    cb.SetAutoMCStats(cb, 10.0)
+    cb.SetAutoMCStats(cb, 0.0)
 
     # Write datacards
     writer = ch.CardWriter(f"{output_dir}/$TAG/$BIN.txt", f"{output_dir}/$TAG/common/htt_input_{era}.root")
